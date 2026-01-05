0ea0d9bf558f63f78f3464fc0bce68b525956302
remyfelicity <99709675+remyfelicity@users.noreply.github.com>
2026-01-05 11:13:32 -0800

    Implement download stats fetching in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,44 @@
+import { createLoader, createParser, type SearchParams } from "nuqs/server";
+import z from "zod";
 import PackageInput from "./PackageInput";
 
-export default function Home() {
+const parseAsPackageNames = createParser({
+  parse: (query) => (query.length === 0 ? null : query.split(",")),
+  serialize: (value) => value.join(","),
+});
+
+const loadSearchParams = createLoader({
+  p: parseAsPackageNames,
+});
+
+const DownloadStatsSchema = z.object({
+  data: z.array(
+    z.object({
+      category: z.string(),
+      date: z.string().transform((date) => new Date(date)),
+      downloads: z.int(),
+    }),
+  ),
+});
+
+async function fetchDownloadStats(packageName: string) {
+  const response = await fetch(
+    `https://pypistats.org/api/packages/${packageName}/overall?mirrors=false`,
+  );
+  const json = await response.json();
+  return DownloadStatsSchema.parse(json);
+}
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
+  const { p } = await loadSearchParams(searchParams);
+  const packageNames = p ?? [];
+
+  const downloadStats = Promise.all(packageNames.map(fetchDownloadStats));
+
   return (
     <div className="mx-auto max-w-4xl px-4">
       <h1 className="flex h-16 items-center text-xl">PyPI Trends</h1>
