b6bf9fe429e937975f8401e4dd6440ff0b1d5389
remyfelicity <99709675+remyfelicity@users.noreply.github.com>
2026-01-07 13:26:28 -0800

    Add margin above chart

--- app/PackageChart.tsx
+++ app/PackageChart.tsx
@@ -21,49 +21,51 @@ export function PackageChart({
   if (!data.size) return null;
 
   return (
-    <VictoryChart
-      padding={{
-        top: 0,
-        left: 40,
-        bottom: 40,
-        right: 0,
-      }}
-      theme={VictoryTheme.clean}
-    >
-      <VictoryAxis
-        crossAxis
-        tickFormat={(timestamp: number) =>
-          new Date(timestamp).toLocaleString("en-US", {
-            day: "numeric",
-            month: "short",
-          })
-        }
-      />
-      <VictoryAxis
-        dependentAxis
-        style={{ grid: { stroke: "oklch(87.2% 0.01 258.338)" } }}
-        tickFormat={(downloadCount: number) =>
-          downloadCount.toLocaleString("en-US", { notation: "compact" })
-        }
-      />
-      {data
-        .entries()
-        .toArray()
-        .map(([packageName, packageData], i) => (
-          <VictoryLine
-            data={packageData.map((packageDatum) => ({
-              x: packageDatum.date,
-              y: packageDatum.downloads,
-            }))}
-            key={packageName}
-            style={{
-              data: {
-                stroke: VictoryTheme.clean.palette?.qualitative?.at(i),
-                strokeWidth: 1,
-              },
-            }}
-          />
-        ))}
-    </VictoryChart>
+    <div className="mt-4">
+      <VictoryChart
+        padding={{
+          top: 0,
+          left: 40,
+          bottom: 40,
+          right: 0,
+        }}
+        theme={VictoryTheme.clean}
+      >
+        <VictoryAxis
+          crossAxis
+          tickFormat={(timestamp: number) =>
+            new Date(timestamp).toLocaleString("en-US", {
+              day: "numeric",
+              month: "short",
+            })
+          }
+        />
+        <VictoryAxis
+          dependentAxis
+          style={{ grid: { stroke: "oklch(87.2% 0.01 258.338)" } }}
+          tickFormat={(downloadCount: number) =>
+            downloadCount.toLocaleString("en-US", { notation: "compact" })
+          }
+        />
+        {data
+          .entries()
+          .toArray()
+          .map(([packageName, packageData], i) => (
+            <VictoryLine
+              data={packageData.map((packageDatum) => ({
+                x: packageDatum.date,
+                y: packageDatum.downloads,
+              }))}
+              key={packageName}
+              style={{
+                data: {
+                  stroke: VictoryTheme.clean.palette?.qualitative?.at(i),
+                  strokeWidth: 1,
+                },
+              }}
+            />
+          ))}
+      </VictoryChart>
+    </div>
   );
 }
