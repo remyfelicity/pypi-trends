f77030f29050b9813f4149d3adb36d783813c8ec
remyfelicity <99709675+remyfelicity@users.noreply.github.com>
2026-01-08 16:15:14 -0800

    Remove Victory

--- app/PackageChart.tsx
+++ app/PackageChart.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { use } from "react";
-import { VictoryAxis, VictoryChart, VictoryLine, VictoryTheme } from "victory";
 
 export function PackageChart({
   data: dataPromise,
@@ -20,61 +19,5 @@ export function PackageChart({
 
   if (!data.size) return null;
 
-  return (
-    <div className="mt-4">
-      <VictoryChart
-        padding={{
-          top: 0,
-          left: 60,
-          bottom: 40,
-          right: 0,
-        }}
-        theme={VictoryTheme.clean}
-      >
-        <VictoryAxis
-          crossAxis
-          style={{
-            grid: { stroke: "oklch(87.2% 0.01 258.338)" },
-            tickLabels: { fontSize: 20 },
-          }}
-          tickCount={3}
-          tickFormat={(timestamp: number) =>
-            new Date(timestamp).toLocaleString("en-US", {
-              day: "numeric",
-              month: "short",
-            })
-          }
-        />
-        <VictoryAxis
-          dependentAxis
-          style={{
-            grid: { stroke: "oklch(87.2% 0.01 258.338)" },
-            tickLabels: { fontSize: 20 },
-          }}
-          tickFormat={(downloadCount: number) =>
-            downloadCount.toLocaleString("en-US", { notation: "compact" })
-          }
-        />
-        {data
-          .entries()
-          .toArray()
-          .map(([packageName, packageData], i) => (
-            <VictoryLine
-              animate={{ duration: 1000 }}
-              data={packageData.map((packageDatum) => ({
-                x: packageDatum.date,
-                y: packageDatum.downloads,
-              }))}
-              key={packageName}
-              style={{
-                data: {
-                  stroke: VictoryTheme.clean.palette?.qualitative?.at(i),
-                  strokeWidth: 2,
-                },
-              }}
-            />
-          ))}
-      </VictoryChart>
-    </div>
-  );
+  return <></>;
 }
